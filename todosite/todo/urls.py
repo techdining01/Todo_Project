bd7cb68f8a51939ab12aea2c4b9c9141d4723fcf
techdining01 <amooade12@gmail.com>
2024-12-01 22:23:59 +0100

    features in todo

--- todosite/todo/urls.py
+++ todosite/todo/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('create-todo/', views.create_todo, name='create-todo'),
+    path('todo/<id>/', views.todo_detail, name='todo')
 ]
--- todosite/todo/views.py
+++ todosite/todo/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
+from .models import Todo
+from .form import TodoForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'todo/home.html')
+    todos = Todo.objects.all()
+    context = {"todos": todos}
+    return render(request, 'todo/home.html', context)
+
+
+def create_todo(request):
+    form = TodoForm()
+    context = {'form':form}
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        desciption = request.POST.get('description')
+        is_completed = request.POST.get('is_completed', False)
+
+        todo = Todo()    
+        todo.title = title
+        todo.description = desciption
+        todo.is_completed = True if is_completed == 'on' else False
+        todo.save()
+
+        return HttpResponseRedirect(reverse('todo', kwargs={'id':todo.pk}))
+        
+    return render(request, 'todo/create_todo.html', context)
+
+def todo_detail(request,id):
+    context = {}
+    return render(request,'todo/todo_detail.html', context)
+
